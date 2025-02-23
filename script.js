29a0dd7477f9798d1b2b16b1fbb6a0240e435d9b
fahdaguenouz <faguenouz@gmail.com>
2025-02-23 18:41:20 +0000

    makke your game

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded",()=>{
+   CreateField()
+
+
+}) 
+
+
+
+function CreateField(){
+    const field=document.createElement("div")
+    const bg=document.createElement("img")
+    const main=document.querySelector("#container")
+    main.appendChild(field) 
+    bg.setAttribute('src', './media/homebg.png');
+    bg.setAttribute('id', 'fieldImg');
+
+    bg.setAttribute('alt', 'Background');
+    bg.setAttribute('height', '100%');
+    bg.setAttribute('width', '100%');
+    main.appendChild(bg)
+}
+    
