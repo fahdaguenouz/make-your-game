783d0cac66fe15ffc88cdd84e23eaed82a94edf2
fahdaguenouz <faguenouz@gmail.com>
2025-03-24 15:57:39 +0000

    setting up the index js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('Working!');
+import {LEVEL,OBJECT_TYPE} from "./setup"
+
+//DOM ELements
+const gameGrid =document.querySelector("#game")
+const scoretable=document.querySelector("#score")
+const startButton =document.querySelector("#start-button")
+//GAME Constants
+const POWER_PILL_TIME=1000
+const GLOBAL_SPEED=80
+
+
+//init setup 
+
+let score=0
+let timer=null
+let gamewin=false
+let powerPillActive=false
+let powerPillTimer=null
+
+function GameOver(pacman,grid){
+
+}
+
+function checkCollision(pacman,grid){
+
+}
+function gameLoop(pacman,ghosts){
+
+}
+
+function startGame(){
+    
+}
