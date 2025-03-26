6bf8b7419d920ec8907cc68f5cf4d42335f5167c
fahdaguenouz <faguenouz@gmail.com>
2025-03-26 14:54:50 +0000

    adding pacman

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import {LEVEL,OBJECT_TYPE} from "./setup.js"
 console.log(LEVEL);
 
 import GameBoard from "./GameBoard.js"
-
+import Pacman from "./Pacman.js";
 //DOM ELements
 const gameGrid =document.querySelector("#game")
 const scoretable=document.querySelector("#score")
@@ -37,5 +37,18 @@ function gameLoop(pacman,ghosts){
 }
 
 function startGame(){
+gamewin=false
+score=0
+powerPillActive=false
+
+startButton.classList.add('hide')
+gameBoard.createGrid(LEVEL)
+const pacman=new Pacman(2,287)
+gameBoard.addObject(287,[OBJECT_TYPE.PACMAN])
+document.addEventListener('keydown',()=>{
+    pacman.handleKeyInput(e,gameBoard.objectExist)
+})
 
 }
+//init game
+startButton.addEventListener("click",startGame)
